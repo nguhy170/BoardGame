7f5197de5d5a0e59017d055527ddaeedf26d7291
nguhy170 <nguhy170@mymail.unisa.edu.au>
2025-11-01 19:59:23 +1030

    Add project files.

---
+++ BoardGame/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        // GET: Home/Details/5
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // GET: Home/Edit/5
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+
+        // GET: Home/Delete/5
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
